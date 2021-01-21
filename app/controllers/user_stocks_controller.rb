06cbe101d3a1b1744f8f400da3b24842576380c0
Igors78 <oleinikovsigors@gmail.com>
2021-01-21 13:42:20 +0200

    Add stock restrictions

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,9 +4,9 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
       stock.save
-      @user_stock = UserStock.create(user: current_user, stock: stock)
-      flash[:notice] = "Stock #{stock.name} added to your portfolio"
-      redirect_to my_portfolio_path
     end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} added to your portfolio"
+    redirect_to my_portfolio_path
   end
 end
