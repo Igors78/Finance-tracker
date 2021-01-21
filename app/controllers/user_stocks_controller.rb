ce576dc07b26fe72ff0b81d08acf692adb4ce380
Igors78 <oleinikovsigors@gmail.com>
2021-01-21 18:28:21 +0200

    Add remove from track function

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.destroy_by(user_id: current_user.id, stock_id: stock.id)
+
+    flash[:notice] = "#{stock.ticker} was removed from track!"
+    redirect_to my_portfolio_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: %i[create destroy]
   get 'stocks/search'
   devise_for :users
   root 'welcome#index'
