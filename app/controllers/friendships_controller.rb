55d2abb89ec72637d9068571d04bf8fa74ecfdd8
Igors78 <oleinikovsigors@gmail.com>
2021-01-23 19:39:19 +0200

    Add create friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,6 +11,17 @@ class FriendshipsController < ApplicationController
     redirect_to friendships_path
   end
 
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = 'Following user'
+    else
+      flash[:alert] = 'Something went wrong!'
+    end
+    redirect_to friendships_path
+  end
+
   def search
     if params[:friend].present?
       @friends = User.search_friend(params[:friend])
